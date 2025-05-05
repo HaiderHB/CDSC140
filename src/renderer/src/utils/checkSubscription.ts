eb7748c365d68383ace3d2d585fceddf0b197610
Haider <shaiderhb@gmail.com>
2025-05-05 19:24:05 -0400

    check sub

--- src/renderer/src/utils/checkSubscription.ts
+++ src/renderer/src/utils/checkSubscription.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 export async function checkSubscriptionStatus(userId: string): Promise<boolean> {
-  return true
   try {
     const response = await axios.post('https://www.interviewspeaker.co/api/check-plan-status', {
       user_id: userId
